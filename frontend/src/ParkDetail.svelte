417c67525dc57ae7ec18f2ee76b5e44f24337a52
EnigmaCurry <ryan@enigmacurry.com>
2026-03-18 04:16:06 +0000

    Fix Leaflet marker icons in ParkDetail component

--- frontend/src/ParkDetail.svelte
+++ frontend/src/ParkDetail.svelte
@@ -2,7 +2,17 @@
   import { onDestroy, tick, createEventDispatcher } from "svelte";
   import L from "leaflet";
   import "leaflet/dist/leaflet.css";
+  import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+  import markerIcon from "leaflet/dist/images/marker-icon.png";
+  import markerShadow from "leaflet/dist/images/marker-shadow.png";
   import { parkAward, parkAwardTitle } from "./parkAward.js";
+
+  delete L.Icon.Default.prototype._getIconUrl;
+  L.Icon.Default.mergeOptions({
+    iconRetinaUrl: markerIcon2x,
+    iconUrl: markerIcon,
+    shadowUrl: markerShadow,
+  });
 
   const dispatch = createEventDispatcher();
 
