165f7d90d826a8a2f539e41f0d0ca11e56e8af9b
EnigmaCurry <ryan@enigmacurry.com>
2026-03-27 18:50:15 +0000

    Revert "Stop auto-selecting first park on My Parks initial load"
    This reverts commit 8b705b502f619e066e4d773a93033f528929b69f.

--- frontend/src/Parks.svelte
+++ frontend/src/Parks.svelte
@@ -481,6 +481,7 @@
     mapResizeObserver = new ResizeObserver(() => { leafletMap?.invalidateSize(); });
     mapResizeObserver.observe(mapEl);
     if (activePark) showActivePark(activePark);
+    else if (myParksSorted.length > 0) selectPark(myParksSorted[0].reference);
     renderingMap = false;
   }
 
