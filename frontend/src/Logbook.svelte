0a663f8f18cd995bc94ec2fd823ba1da95550dfb
EnigmaCurry <ryan@enigmacurry.com>
2026-05-13 17:58:19 +0000

    Use text input with decimal filter for TX power

--- frontend/src/Logbook.svelte
+++ frontend/src/Logbook.svelte
@@ -1226,7 +1226,7 @@
     </div>
     <div class="field" class:changed={orig && tx_pwr !== orig.tx_pwr}>
       <label for="tx_pwr">TX Pwr (W)</label>
-      <input id="tx_pwr" type="number" bind:value={tx_pwr} min="0" step="any" />
+      <input id="tx_pwr" type="text" inputmode="decimal" bind:value={tx_pwr} on:input={() => { tx_pwr = tx_pwr.replace(/[^0-9.]/g, "").replace(/(\..*)\./g, "$1"); }} />
     </div>
     <div class="field field-name" class:changed={orig && name !== orig.name}>
       <label for="name">Name</label>
